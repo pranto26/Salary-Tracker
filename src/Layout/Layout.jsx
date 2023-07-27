2b1970899570e6e676efe50921304298952413d8
Pranto Saha <prantosaha604@gmail.com>
2023-07-27 19:41:37 +0600

    Responsive Design Added

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -11,9 +11,10 @@ const Layout = (props) => {
       <label tabIndex={0} className="btn btn-ghost lg:hidden">
         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
       </label>
-      <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-        <li><a>Item 1</a></li>
-        <li><a>Item 2</a></li>
+      <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-dark text-black rounded-box w-52">
+      <li><NavLink to={"/"}>Home</NavLink></li>
+      <li><NavLink to={"/income"}>Income</NavLink></li>
+      <li><NavLink to={"/expanse"}>Expanse</NavLink></li>
       </ul>
     </div>
     <a className="btn btn-ghost normal-case text-xl">Salary Tracker</a>
